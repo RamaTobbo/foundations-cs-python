e6ed4e1239587e44eef8babb20d46d6e72d1e2fb
Rama Tobbo <ramatobbo5@gmail.com>
2023-10-28 11:11:17 +0200

    invert a dictionary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     name=input("enter your name: ")
-    print(f"welcom {name}")
+    print(f"welcome {name}")
     while True:
 
         print("""1.Add Matrices
@@ -15,9 +15,9 @@ def main():
         if choice =='1':
            add_matrices()
         elif choice == '2':
-            return 0
+          is_rotation([1,2,3],[4,5,6])
         elif choice == '3':
-            return 0
+           invert_dictionary()
         elif choice == '4':
             return 0
         elif choice == '5':
@@ -58,5 +58,19 @@ def add_matrices():
      for row in result:
          print(row)
 
-
+def is_rotation(m1,m2):
+    rows=1
+def invert_dictionary():
+    original={}
+    inverted={}
+    items=int(input("Enter the number of items in the dictionary:"))
+    for i in range(items):
+        key=input("Enter a key: ")
+        value=input("Enter a value:")
+        original[key]=value
+    for key,value in original.items():
+        if value in inverted:
+            inverted[value]=key
+    print(original)
+    print(inverted)
 main()
