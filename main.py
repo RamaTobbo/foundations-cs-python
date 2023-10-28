24090ea2ab73735985f3333d887d25e74c20deb1
Rama Tobbo <ramatobbo5@gmail.com>
2023-10-28 11:31:11 +0200

    fixing the indexing

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def is_palindrome(s):
     if len(s)<=1:
         return True
     if s[0]==s[-1]:
-        return is_palindrome(s[1:0])
+        return is_palindrome(s[1:-1])
     else:
         return False
 main()
