65ae169152d3c99532113992f45d644aecae6364
Rama Tobbo <ramatobbo5@gmail.com>
2023-10-28 10:26:13 +0200

    creating the main

---
+++ main.py
@@ -0,0 +1,36 @@
+def main():
+    name=input("enter your name: ")
+    print(f"welcom {name}")
+    while True:
+
+        print("""1.Add Matrices
+             2. Check Rotation
+             3. Invert Dictionary
+             4. Convert Matrix to Dictionary
+             5. Check Palindrome
+             6. Search for an Element & Merge Sort
+             7. Exit
+    """)
+    choice=input("Choose a number between (1-7):")
+    if choice =='1':
+        return 0
+    elif choice=='2':
+        return 0
+    elif choice == '3':
+        return 0
+    elif choice == '4':
+        return 0
+    elif choice == '5':
+        return 0
+    elif choice == '6':
+        return 0
+    elif choice == '7':
+        exit(0)
+    else:
+        print("Enter a valid option")
+
+
+
+
+
+
