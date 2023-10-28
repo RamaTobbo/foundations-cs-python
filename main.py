beaa19d8fee15c30f7d1a88b4ce099e20a874c7e
Rama Tobbo <ramatobbo5@gmail.com>
2023-10-28 10:46:06 +0200

    making the add matrices

--- main.py
+++ main.py
@@ -29,8 +29,31 @@ def main():
     else:
         print("Enter a valid option")
 
-
-
+def add_matrices:
+     rows = int(input("Enter number of rows: "))
+     cols=int(input("Enter number of cols: "))
+     m1=[]
+     m2=[]
+     print("Enter elements for the first matrix:")
+     for i in range(rows):
+         row=[]
+         for j in range(cols):
+             nbs=int(input("enter number at row {i+1},column {j+1}:"))
+             row.append(nbs)
+         m1.append(row)
+     print("Enter elements for the second matrix:")
+     for i in range(rows):
+         row=[]
+         for j in range(cols):
+             nbs=int(input("enter number at row {i+1},column {j+1}:"))
+             row.append(nbs)
+         m2.append(row)
+     result=[]
+     for i in range(rows):
+         row=[]
+         for j in range(cols):
+             row.append(m1[i][j]+m2[i][j])
+         result.append(row)
 
 
 
