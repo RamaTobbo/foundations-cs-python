9742f16e4f3443ba3859551ab01dad2da7b1034a
Rama Tobbo <ramatobbo5@gmail.com>
2023-11-11 12:25:00 +0200

    creating the main

--- main.py
+++ main.py
@@ -124,3 +124,47 @@ def importTabs(file_path):
              tabs = json.load(file)
     print("Tabs loaded successfully.")
 
+
+def main():
+    user=input("Enter your name :")
+    print(f"welcome {user}")
+    while True:
+        print("\nMenu:")
+        print("1. Open Tab")
+        print("2. Close Tab")
+        print("3. Switch Tab")
+        print("4. Display All Tabs")
+        print("5. Open Nested Tab")
+        print("6. Clear All Tabs")
+        print("7. Save Tabs")
+        print("8. Import Tabs")
+        print("9. Exit")
+
+        choice = input("Enter your choice (1-9): ").strip()
+
+        if choice == '1':
+            openTab()
+        elif choice == '2':
+            closeTab()
+        elif choice == '3':
+            switchTab()
+        elif choice == '4':
+            pass
+        elif choice == '5':
+            openNestedTab()
+            print(tabs)
+        elif choice == '6':
+            clearAllTabs()
+        elif choice == '7':
+            saveTabs()
+        elif choice == '8':
+            file_path = input("Enter the file path to import tabs: ").strip()
+            loaded = importTabs(file_path)
+
+
+        elif choice == '9':
+            print("Exiting the program. ")
+            break
+        else:
+            print("Invalid choice. ")
+
